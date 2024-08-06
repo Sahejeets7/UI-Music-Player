16d8887bdfa70434e9e226066eafc337d0f8a809
Sahejeets7 <31505949+Sahejeets7@users.noreply.github.com>
2024-08-07 00:43:07 +0530

    chore: new eslint code improvements

--- src/util.js
+++ src/util.js
@@ -1,12 +1,20 @@
+/* eslint-disable no-unused-vars */
 export const playAudio = (isPlaying, audioRef) => {
   if (isPlaying) {
+    console.log('PLayAudio = isPlaying -> ', isPlaying);
+    console.log('Utils making a promise for audio', audioRef.current);
     const playPromise = audioRef.current.play();
-    if (playPromise !== undefined) {
+    console.log('playPromise', playPromise);
+
+    if (playPromise != null) {
       playPromise
-        .then((audio) => {
+        .then(_ => {
+          audioRef.current.pause();
           audioRef.current.play();
         })
-        .catch((error) => console.log(error));
+        .catch(error => console.log(error));
+    } else {
+      console.log('promise is null');
     }
   }
 };
