7c568cafe1ea8ef4dcf1480546ce3d8d2cdd2b04
Sahejeets7 <Sahejeetsingh97@gmail.com>
2025-06-15 06:05:57 +0530

    fix: update audio links to new sources

--- src/data.js
+++ src/data.js
@@ -7,7 +7,8 @@ function chillHop() {
       cover:
         'https://cms.chillhop.com/media/1095/squarel4a03d3bcc979a6fc894b95ce39d00e0909d1d3f0.jpg',
       artist: 'Aso, Middle School, Aviino',
-      audio: 'https://mp3.chillhop.com/serve.php/?mp3=10075',
+      audio:
+        'https://pixabay.com/music/acoustic-group-morning-garden-acoustic-chill-15013/',
       color: ['#205950', '#2ab3bf'],
       id: uuidv4(),
       active: true,
@@ -17,7 +18,8 @@ function chillHop() {
       cover:
         'https://cdn.prod.website-files.com/5fc4bca9bd65e8f2bf87f576/63fba7bd3efb7261d24add42_Square-2%201-p-500.webp',
       artist: 'Aiguille',
-      audio: 'https://mp3.chillhop.com/serve.php/?mp3=9272',
+      audio:
+        'https://pixabay.com/music/beats-good-night-lofi-cozy-chill-music-160166/',
       color: ['#EF8EA9', '#ab417f'],
       id: uuidv4(),
       active: false,
@@ -27,7 +29,7 @@ function chillHop() {
       cover:
         'https://cms.chillhop.com/media/77075/squareld9736ad4c1782864c32ff9eef792c8d09a01e825.jpg',
       artist: 'Swørn',
-      audio: 'https://mp3.chillhop.com/serve.php/?mp3=9222',
+      audio: 'https://pixabay.com/music/upbeat-chill-beats-185843/',
       color: ['#CD607D', '#c94043'],
       id: uuidv4(),
       active: false,
@@ -37,7 +39,7 @@ function chillHop() {
       cover:
         'https://cms.chillhop.com/media/621/squarel774a0958326fec3fd21e32a500287729179d12de.jpg',
       artist: 'Aiguille',
-      audio: 'https://mp3.chillhop.com/serve.php/?mp3=9148',
+      audio: 'https://pixabay.com/music/beats-chill-lofi-316579/',
       color: ['#EF8EA9', '#ab417f'],
       id: uuidv4(),
       active: false,
@@ -47,7 +49,7 @@ function chillHop() {
       cover:
         'https://cms.chillhop.com/media/6816/squarelff35dede32321a8aa0953809812941bcf8a6bd35.jpg',
       artist: 'Swørn',
-      audio: 'https://mp3.chillhop.com/serve.php/?mp3=9228',
+      audio: 'https://pixabay.com/music/upbeat-upbeat-background-music-315196/',
       color: ['#CD607D', '#c94043'],
       id: uuidv4(),
       active: false,
@@ -57,7 +59,8 @@ function chillHop() {
       cover:
         'https://cms.chillhop.com/media/69928/squarele3a51ef1515665232dae9c9d02d3689d2e7611ff.jpg',
       artist: 'Aso, Middle School, Aviino',
-      audio: 'https://mp3.chillhop.com/serve.php/?mp3=10074',
+      audio:
+        'https://pixabay.com/music/beats-lazy-day-stylish-futuristic-chill-239287/',
       color: ['#205950', '#2ab3bf'],
       id: uuidv4(),
       active: false,
